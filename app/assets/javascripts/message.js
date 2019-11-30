5433eea9890637693cccaa5ff17e536ffe9839fb
MasanoriNishizawa <57927373+MasanriNishizawa@users.noreply.github.com>
2019-11-30 17:25:24 +0900

    JS修正1

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,27 @@
 $(function(){ 
   function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="message__upper-info">
-           <div class="message__upper-info__talker">
+    var img = "";
+    if ( message.image ) {
+    var img = '<img src=${message.image} class: "lower-message__image">'
+    }
+    var html =`<div class="message" data-message-id=${message.id}>
+         <div class="upper-message">
+           <div class="upper-message__user-name">
              ${message.user_name}
            </div>
-           <div class="message__upper-info__date">
+           <div class="upper-message__date">
              ${message.date}
            </div>
          </div>
-         <div class="message__text">
-           <p class="lower-message__image">
+         <div class="lower-message">
+           <p class="lower-message__content">
              ${message.content}
            </p>
          </div>
-         <img src=${message.image} >
+         
        </div>`
      return html;
-   } else {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="message__upper-info">
-           <div class="message__upper-info__talker">
-             ${message.user_name}
-           </div>
-           <div class="message__upper-info__date">
-             ${message.date}
-           </div>
-         </div>
-         <div class="message__text">
-           <p class="lower-message__image">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
- }
+   }
 $('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
